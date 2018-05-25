6a94c5349b40d47229fdc8c10ab95518b30f55a0
Brannon Laveder <blaveder1@gmail.com>
2018-05-25 17:00:45 -0600

    test

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,24 +1,69 @@
 
 $(document).ready(function () {
     var standardList = ["kitty", "puppy", "cockatoo", "bird", "funny", "donald Trump"];
-    var dynamic = ["maybe"];
-    var buttonContainer = $("#button-container");
+
+
 
 
 
     function buttonGenerator(firstList) {
-
+        var buttonContainer = $("#button-container");
         $("#button-container").empty();
 
         for (var i = 0; i < firstList.length; i++) {
             var button = $("<input type='button'>");
 
             buttonContainer.append(button);
-            button.attr({ value: firstList[i], class: "buttons" });
+            button.attr({ value: firstList[i], class: "btn btnSp btn-light" });
         }
         if (firstList.length == 20) {
             firstList.splice(5, firstList.length)
         }
+
+
+        $(".btn").on("click", function () {
+
+            var person = $(this).attr("value");
+            var offset = Math.floor((Math.random() * 10) * 10);
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=Nd2l5e3g2sVHIOCLQfXIKRY2OIutniDC&limit=12&offset=" + offset + '"';
+
+            console.log(person);
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                var results = response.data;
+                console.log(results);
+                var cardClass = "card";
+                $("#gif-containers").empty();
+                for (var i = 0; i < results.length; i++) {
+                    var cards = $("#gif-containers");
+                    var cardColumn = $("<div class='col-lg-3 col-xs-3'>");
+                    var carDiv = $("<div class='card'>");
+                    var cardBody = $("<div class='card-body'>");
+                    var h5 = $("<h5 class='card-title'>");
+                    var p = $("<p class='card-text'>");
+                    p.text(results[i].title);
+                    cards.attr("class", "row test");
+                    cards.append(cardColumn);
+                    cardColumn.append(carDiv);
+                    carDiv.append("<img src=" + results[i].images.fixed_height.url + ' ' + 'class=' + '"' + 'card-img-top' + '"' + "/>");
+
+                    carDiv.append(cardBody);
+                    cardBody.append(h5);
+                    cardBody.append(p);
+
+
+                    h5.text("Rating: " + results[i].rating);
+
+
+
+
+                    $("#gif-container").addClass("test");
+                }
+            });
+
+        });
     }
     buttonGenerator(standardList);
 
@@ -26,29 +71,23 @@ $(document).ready(function () {
 
         event.preventDefault();
 
-        var userInput = $("#user-input").val().trim();
-        console.log(userInput);
-        console.log(standardList);
+        var userInput = $("#user-input").val();
+        // console.log(userInput);
+        // console.log(standardList);
         standardList.push(userInput);
         buttonGenerator(standardList);
         $("#user-input").val("");
+        $("#user-input").empty();
 
-        // var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     console.log(response);
+        if (userInput === "") {
+            event.preventDefault();
 
-
-
-
-        // });buttonGenerator(standardList);
+            standardList.pop(-1);
+        }
     });
-    $("#user-input").empty();
+
+
 });
-
-
 
 
 
